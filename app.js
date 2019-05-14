a717496ce37211d247e56f2c35bb2dc3e31b3596
bkauf@google.com <bkauf@google.com>
2019-05-14 15:15:49 -0400

    lots of changes

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ var request      = require('request');
 var fs           = require("fs");
 const fileUpload = require('express-fileupload');
 var express      = require('express');
+var sizeOf       = require('image-size');
 var app          = express();
 const port       = 8080;
 const automl = require('@google-cloud/automl');
+const sharp  = require('sharp');
+
 const client = new automl.PredictionServiceClient({
   projectId: 'bkauf-sandbox',
   keyFilename: '/usr/src/app/bkauf-sandbox.json',
@@ -12,7 +15,9 @@ const client = new automl.PredictionServiceClient({
 
 const project = 'bkauf-sandbox';
 const region = 'us-central1';
-const automlModel = 'ICN973436885879248128';
+//const automlModel = 'ICN973436885879248128';//classification
+const automlModel = 'IOD822197203064848384';//object
+
  //filePath = 'GCS_PATH',
 process.env.GOOGLE_APPLICATION_CREDENTIALS ='/usr/src/app/bkauf-sandbox.json';
 
@@ -32,65 +37,116 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 
-app.use(function(req, res, next) {
+/* app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
+*/
 
 app.use(fileUpload());
 app.post('/automl',function(req,res){
-
-
+    var timeStamp = Date.now();
+    var uploadPath = './public/images/uploads/raw/'+timeStamp+'.jpg';
+    var resizePath = './public/images/uploads/resize/'+timeStamp+'.jpg';
     if (Object.keys(req.files).length == 0) {
        return res.status(400).send('No files were uploaded.');
      }
-
      // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
      let sampleFile = req.files.imageupload;
 
      // Use the mv() method to place the file somewhere on your server
-     sampleFile.mv('./public/images/filename.jpg', function(err) {
+     sampleFile.mv(uploadPath , function(err) {
        if (err)
       //   return res.status(500).send(err);
-    console.log('File uploaded!');
-    console.log('enter predict');
-    var filePath = './public/images/filename.jpg';
-    var bitmap = fs.readFileSync(filePath);
-  //const content  = fs.readFileSync(filePath, 'base64');
-  const content   = new Buffer(bitmap).toString('base64');
-  // Set the payload by giving the content and type of the file.
-    const payload = {};
-    payload.image = {imageBytes: content};
+         console.log('File uploaded!');
+         let inputFile    = uploadPath ;
+         let outputReFile = resizePath;
+         var resize       = "false";
+         var dimensions   = sizeOf(inputFile);
+         newWidth     = 0;
+         newHeight    = 0;
+         var aspectRatio  = Number(dimensions.width)/Number(dimensions.height);
+      console.log("Aspect Ratio:"+aspectRatio );
+         if (Number(dimensions.width)>500 || Number(dimensions.height>500) ){
+                 resize = "yes";
+                 console.log("Resize Image"+dimensions.width+" "+dimensions.height);
+             if (Number(dimensions.height)>Number(dimensions.width)){//calculate larger side
+               newWidth   = Math.round((499 * aspectRatio));
+               newHeight  = 499;
 
-    const formattedName = client.modelPath(project, region, automlModel);
-    const request = {
-      name: formattedName,
-      payload: payload,
-    };
-    client.predict(request)
-    .then(responses => {
-      const response = responses[0];
-    console.log(response.payload[0]);
-  //  res.send(JSON.stringify(response));
-  var message = "YES YES Peace Pinot!";
-  if (response.payload[0].displayName != 'wine_emoji'){
-    var message = "NO, Not Peace Pinot :("
-  }
-    console.log(response.payload[0].classification.score);
-    res.render('results', { title: message, imagesrc:'images/filename.jpg'});
+             }else{
+               newHeight = Math.round((499/aspectRatio));
+               newWidth  = 499;
+               console.log("Test2 dimentions"+newWidth+newHeight);
+             }
+        }else{
+             var newWidth  = dimensions.width;
+             var newHeight = dimensions.height;
+        }
+            console.log("Test dimentions"+newWidth+newHeight);
+            imageResize(inputFile,outputReFile,newWidth,newHeight,resize, function(outputFile) {
+                  console.log('enter predict');
+                        console.log(outputFile);
+                        var bitmap = fs.readFileSync(outputFile);
+                      //const content  = fs.readFileSync(filePath, 'base64');
+                      const content   = new Buffer(bitmap).toString('base64');
+                      // Set the payload by giving the content and type of the file.
+                        const payload = {};
+                        payload.image = {imageBytes: content};
+
+                        const formattedName = client.modelPath(project, region, automlModel);
+                        const request = {
+                          name: formattedName,
+                          payload: payload,
+                        };
+                        client.predict(request)
+                        .then(responses => {
+                          const response = responses[0];
+                      //  console.log(response.payload[0].displayName);
+                      //  res.send(JSON.stringify(response));
+
+                      var message = "NO, Not Peace Pinot :(";
+                      var xycords     = [{x:0,y:0},{x:0,y: 0}];
+                      var automlScore = 0;
+
+                      if (typeof response.payload[0] !== 'undefined') {
+                        var xycords      = response.payload[0].imageObjectDetection.boundingBox['normalizedVertices'];
+                        var automlScore  = response.payload[0].imageObjectDetection.score;
+                        var message      = "YES YES Peace Pinot!";
+                        console.log("Peace Pinot!");
+                      //console.log("Cords:"+JSON.stringify(xycords));
+                        console.log("Score:"+automlScore);
+                        //console.log("Height:"+dimensions.height);
+                        //console.log("Width:"+dimensions.width);
+
+                      }
+
+                        fileNameArry = outputFile.split("/");
+                        filePath = fileNameArry[4];
+                        fileName = fileNameArry[5];
+
+                        var dimensions = sizeOf(outputFile);
+
+                        res.render('results', { imgWidth:newWidth,imgHeight:newHeight, title: message, imagesrc:'images/uploads/'+filePath+"/"+fileName,xycords:JSON.stringify(xycords), score: automlScore,imageWidth: newWidth, imageHeight: newHeight});
+                            delete newWidth;
+                            delete newHeight;
 
 
-      })
-      .catch(err => {
-        console.error(err);
-      });
+                        })
+                          .catch(err => {
+                            console.error(err);
+                          });
 
-      //fs.unlink('./uploads/filename.jpg');
+
+            });//end of resize callbacl
+
+      });//end file upload callback
+
       console.log('automl completed');
 
     });
-});
+
 
 
 
@@ -115,3 +171,30 @@ app.use(function(err, req, res, next) {
 
 module.exports = app;
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+
+function imageResize(inputFile,outputFile,newWidth,newHeight,resize,callback){
+    if (resize == "yes"){
+
+        console.log("starting dimensions:"+JSON.stringify(sizeOf(inputFile)));
+        console.log("To: "+newWidth+" "+newHeight);
+         sharp(inputFile).resize({ width: newWidth,height: newHeight}).toFile(outputFile)
+               .then(function(newFileInfo){
+                   // newFileInfo holds the output file properties
+                   console.log("Image Resized:"+JSON.stringify(newFileInfo));
+                   //fs.unlink('./public/images/uploads/filenameRaw.jpg');
+                   callback(outputFile);
+
+                })
+                .catch(function(err) {
+                       console.log("Error occured Resizing Image Width"+err);
+                });
+
+
+  }else{
+  //  return  inputFile;
+      callback(inputFile);
+  }
+  //  return   outputFile;
+
+}
