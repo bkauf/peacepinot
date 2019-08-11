041f777edc232a7fe2607d3366a6efaf1f8377a2
Brian Kaufman <brian.kaufman@gmail.com>
2019-08-11 09:33:11 -0400

    Create loaderio.js

---
+++ routes/loaderio.js
@@ -0,0 +1,9 @@
+var express  = require('express');
+var router   = express.Router();
+
+router.get('/', function(req, res, next) {
+   res.send('loaderio-7fb93f7a58f56efbca84be04d559c29d');
+
+});
+
+module.exports = router;
