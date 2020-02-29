5c19a201ee04abeae39ebe0ee7f36bc9d055a2c5
bkauf@google.com <bkauf@google.com>
2020-02-29 15:28:55 -0500

    build details

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,14 @@ var express  = require('express');
 var router   = express.Router();
 var os       = require("os");
 var hostname = os.hostname();
+var fs       = require('fs');
 /* GET home page. */
 router.get('/', function(req, res, next) {
+   fs.readFile('buildDetails', 'utf8', function(err, contents) {
+    var buildDts = JSON.parse(contents);
 
-
-  res.render('index', { title: ':Peace Pinot:', container: hostname });
+  res.render('index', { title: ':Peace Pinot:', container: hostname,buildID: buildDts.buildID, commitID: buildDts.commitID  });
+  });
 });
 
 module.exports = router;
