4abc760477e25cd91e099d13c4923a061c9a08ff
bkauf@google.com <bkauf@google.com>
2020-03-02 13:34:22 -0500

    change of app for cr and k8s support

--- app.js
+++ app.js
@@ -19,16 +19,29 @@ const region      = 'us-central1';//region of autoML model
 const automlModel = 'IOD822197203064848384';//object
 // end account specifc variables
 
-// Comment out for 3/2 event
-const client = new automl.PredictionServiceClient({
-  projectId: project//,
-//  keyFilename: saToken, //taken out for cloud run specific access
-});
 
-const storage = new Storage({
-  projectId: project//,
-//  keyFilename: saToken ////taken out for cloud run specific access 
-});
+if(saToken!=''){// this is running in k8s
+    const client = new automl.PredictionServiceClient({
+      projectId: project,
+      keyFilename: saToken
+    });
+
+    const storage = new Storage({
+      projectId: project,
+      keyFilename: saToken
+    });
+
+}else{ // this is running in cloud run
+    const client = new automl.PredictionServiceClient({
+      projectId: project//,
+    //  keyFilename: saToken, //taken out for cloud run specific access
+    });
+
+    const storage = new Storage({
+      projectId: project//,
+    //  keyFilename: saToken ////taken out for cloud run specific access
+    });
+}
 //end
 var path         = require('path');
 var favicon      = require('serve-favicon');
